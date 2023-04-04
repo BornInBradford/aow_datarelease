f3b50b10372972443cad9b6c9901eaff16512ac7
dan.mason@bthft.nhs.uk <dan.mason@bthft.nhs.uk>
2023-04-04 15:55:51 +0100

    Some denominator cleaning

--- denom/load_and_tidy_denominator_inputs.R
+++ denom/load_and_tidy_denominator_inputs.R
@@ -1,8 +1,35 @@
 
 library(haven)
+library(dplyr)
 
 input_path <- "U:/Born in Bradford - AOW Raw Data/sql/denominator/data/"
 
 consent <- read_dta(file.path(input_path, "AOW_Consent.dta"))
 schoolrec <- read_dta(file.path(input_path, "AOW_School_RecruitmentList.dta"))
+
+# check duplicates
+consent$AoWRecruitmentID[duplicated(consent$AoWRecruitmentID) |> which()]
+schoolrec$AoWRecruitmentID[duplicated(schoolrec$AoWRecruitmentID) |> which()]
+
+# can they be joined one-to-one
+nrow(consent)
+nrow(schoolrec)
+denom_all <- consent |> inner_join(schoolrec, by = "AoWRecruitmentID", suffix = c("_con", "_rec"))
+nrow(denom_all)
+
+# check conflicts between school recruitment and consent tables
+conflicts <- function(x, y) {
+  con <- which(x != y)
+  output <- paste0(x[con], " : ", y[con])
+  return(output)
+}
+
+conflicts(denom_all$EstablishmentNumber_con, denom_all$EstablishmentNumber_rec) |> head()
+conflicts(denom_all$School_con, denom_all$School_rec) |> head()
+conflicts(denom_all$YearGroup_con, denom_all$YearGroup_rec) |> head()
+conflicts(denom_all$FormTutor_con, denom_all$FormTutor_rec) |> head()
+conflicts(denom_all$CreatedDateTime_con, denom_all$CreatedDateTime_rec) |> head()
+conflicts(denom_all$ModifiedDateTime_con, denom_all$ModifiedDateTime_rec) |> head()
+
+
 
