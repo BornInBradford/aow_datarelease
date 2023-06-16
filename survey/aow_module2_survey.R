aaec56c305dc1a7c316c19627fd9a41806f99f3d
masond <dan.mason@bthft.nhs.uk>
2023-06-16 17:16:00 +0100

    Further work tagging revised variables in mod 2

--- survey/aow_module2_survey.R
+++ survey/aow_module2_survey.R
@@ -1,6 +1,4 @@
-# Appending AoW Online and Offline Module 1 Survey
-# Amy Hough
-# 04/04/2023
+# Module 2 survey online/offline merge and tidy up
 
 # packages
 library(tidyverse)
@@ -8,31 +6,48 @@ library(haven)
 library(labelled)
 
 # revised or added varname regexp
-ra_exp <- "_[ar][0-9]{1,2}$|_[ar][0-9]{1,2}__[0-9]{1,3}$"
+ra_exp <- "_[ar][0-9]{1,2}$|_[ar][0-9]{1,2}___[0-9]{1,3}$"
+# revised or added select-one varname regexp
+ra_exp_1 <- "_[ar][0-9]{1,2}$"
+# revised or added select-any varname regexp
+ra_exp_n <- "_[ar][0-9]{1,2}___[0-9]{1,3}$"
 # revised varname regexp
-r_exp <- "_r[0-9]{1,2}$|_r[0-9]{1,2}__[0-9]{1,3}$"
+r_exp <- "_r[0-9]{1,2}$|_r[0-9]{1,2}___[0-9]{1,3}$"
+# revised select-one varname regexp
+r_exp_1 <- "_r[0-9]{1,2}$"
+# revised select-any varname regexp
+r_exp_n <- "_r[0-9]{1,2}___[0-9]{1,3}$"
 # added varname regexp
-a_exp <- "_a[0-9]{1,2}$|_a[0-9]{1,2}__[0-9]{1,3}$"
+a_exp <- "_a[0-9]{1,2}$|_a[0-9]{1,2}___[0-9]{1,3}$"
+# added select-one varname regexp
+a_exp_1 <- "_a[0-9]{1,2}$"
+# added select-any varname regexp
+a_exp_n <- "_a[0-9]{1,2}___[0-9]{1,3}$"
 
+# new data dictionary column names
+dict_name <- c("variable", "form", "section", "type", "label", "categories", "note", "validation_type", "validation_min", "validation_max",
+               "identifier", "branching", "required", "alignment", "question_num", "matrix_name", "matrix_rank", "annotation")
 # data
 online <- read_dta("U:\\Born in Bradford - AOW Raw Data\\redcap\\surveys\\data\\tmpSurvey_Module2_Online.dta")
 offline <- read_dta("U:\\Born in Bradford - AOW Raw Data\\redcap\\surveys\\data\\tmpSurvey_Module2_Offline.dta")
 
 # data dictionary
-online_dict <- read_csv("U:\\Born in Bradford - AOW Raw Data\\redcap\\surveys\\data_dictionary\\AoWModule2OnlineSurvey_DataDictionary_2023-06-15.csv")
-offline_dict <- read_csv("U:\\Born in Bradford - AOW Raw Data\\redcap\\surveys\\data_dictionary\\AoWModule2OfflineForm_DataDictionary_2023-06-15.csv")
+online_dict <- read_csv("U:\\Born in Bradford - AOW Raw Data\\redcap\\surveys\\data_dictionary\\AoWModule2OnlineSurvey_DataDictionary_2023-06-15.csv",
+                        col_names = dict_name)
+offline_dict <- read_csv("U:\\Born in Bradford - AOW Raw Data\\redcap\\surveys\\data_dictionary\\AoWModule2OfflineForm_DataDictionary_2023-06-15.csv",
+                         col_names = dict_name)
 
 # which columns are in online but not offline
-online_only <- online_dict |> anti_join(offline_dict, by = "Variable / Field Name")
-offline_only <- offline_dict |> anti_join(online_dict, by = "Variable / Field Name")
+online_only <- online_dict |> anti_join(offline_dict, by = "variable")
+offline_only <- offline_dict |> anti_join(online_dict, by = "variable")
 
 # which columns have value label conflicts
-vlabel_conflict <- online_dict |> inner_join(select(offline_dict, `Variable / Field Name`, off_labs = `Choices, Calculations, OR Slider Labels`),
-                                             by = "Variable / Field Name") |>
-  filter(`Choices, Calculations, OR Slider Labels` != off_labs) |>
-  select(`Variable / Field Name`,
-         on_labs = `Choices, Calculations, OR Slider Labels`,
-         off_labs)
+vlabel_conflict <- online_dict |> inner_join(select(offline_dict, variable, off_categories = categories),
+                                             by = "variable") |>
+  filter(categories != off_categories) |>
+  select(variable,
+         on_categories = categories,
+         off_categories)
 
 # there's an extra space in one of the offline labels so this is fine
 
@@ -47,10 +62,27 @@ mod2_allcols <- online |> bind_rows(offline)
 warnings()
 
 # get revised/added column names
-revadd_offline <- offline_dict |> filter(`Variable / Field Name` %in% grep(ra_exp, online_dict$`Variable / Field Name`, value = TRUE))
-revadd_online <- online_dict |> filter(`Variable / Field Name` %in% grep(ra_exp, offline_dict$`Variable / Field Name`, value = TRUE))
-rev_offline <- offline_dict |> filter(`Variable / Field Name` %in% grep(r_exp, online_dict$`Variable / Field Name`, value = TRUE))
-rev_online <- online_dict |> filter(`Variable / Field Name` %in% grep(r_exp, offline_dict$`Variable / Field Name`, value = TRUE))
-add_offline <- offline_dict |> filter(`Variable / Field Name` %in% grep(a_exp, online_dict$`Variable / Field Name`, value = TRUE))
-add_online <- online_dict |> filter(`Variable / Field Name` %in% grep(a_exp, offline_dict$`Variable / Field Name`, value = TRUE))
+rev_offline <- offline_dict |> filter(variable %in% grep(r_exp, online_dict$variable, value = TRUE))
+rev_online <- online_dict |> filter(variable %in% grep(r_exp, offline_dict$variable, value = TRUE))
+add_offline <- offline_dict |> filter(variable %in% grep(a_exp, online_dict$variable, value = TRUE))
+add_online <- online_dict |> filter(variable %in% grep(a_exp, offline_dict$variable, value = TRUE))
 
+
+# the second expression using ?_exp_n would only be needed if processing actual variable names
+# it removes the ___n from the end of the name
+# only the stem is in the data dictionary i.e. not including the ___ and checkbox value
+online_dict <- online_dict |> mutate(added = case_when(grepl(a_exp_1, variable) ~ substr(variable, nchar(variable), nchar(variable)),
+                                                       grepl(a_exp_n, variable) ~ str_split(variable, "_") |> tail(n=4) |> head(n=1) |> str_replace("\\D", "")),
+                                     revised = case_when(grepl(r_exp_1, variable) ~ substr(variable, nchar(variable), nchar(variable)),
+                                                         grepl(r_exp_n, variable) ~ str_split(variable, "_") |> tail(n=4) |> head(n=1) |> str_replace("\\D", ""))
+)
+offline_dict <- offline_dict |> mutate(added = case_when(grepl(a_exp_1, variable) ~ substr(variable, nchar(variable), nchar(variable)),
+                                                         grepl(a_exp_n, variable) ~ str_split(variable, "_") |> tail(n=4) |> head(n=1) |> str_replace("\\D", "")),
+                                       revised = case_when(grepl(r_exp_1, variable) ~ substr(variable, nchar(variable), nchar(variable)),
+                                                           grepl(r_exp_n, variable) ~ str_split(variable, "_") |> tail(n=4) |> head(n=1) |> str_replace("\\D", ""))
+)
+
+# add hidden column for version var is hidden from
+# process categories to add missing values for added from, revised from, removed from
+# for checkbox variables add a new variable named for the stem just containing the added/revised/hidden categories
+# maybe add a present catregory to these ones
