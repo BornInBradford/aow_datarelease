ce2529e349f7fc41d589312f4e0648e0abee2ca1
dan.mason@bthft.nhs.uk <dan.mason@bthft.nhs.uk>
2023-06-23 17:41:00 +0100

    Added template dataframe summary markdown report and example rendering script

---
+++ survey/example_dataframe_summary_report_render.R
@@ -0,0 +1,9 @@
+# example dataframe summary report rendering
+
+data_frame_file <- "U:/Born In Bradford - Confidential/Data/BiB/processing/AoW/survey/data/aow_survey_module4_linked.rds"
+data_frame_name <- "Survey module 4 linked data frame"
+
+  
+rmarkdown::render("tools/describe_data_frame.Rmd", 
+                  params = list(df_file = data_frame_file, df_name = data_frame_name),
+                  output_file=paste0("../reports/", gsub(" ", "_", tolower(data_frame_name)), ".html"))
