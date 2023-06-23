12943022c97ca82433839279f70f2d2768c5d3fe
dan.mason@bthft.nhs.uk <dan.mason@bthft.nhs.uk>
2023-06-23 11:04:15 +0100

    Reorganising

--- survey/aow_module1_full_pipeline.R
+++ survey/aow_module1_full_pipeline.R
@@ -2,8 +2,8 @@
 
 
 # 1. merge online/offline versions and tag missing values according to survey version where possible
-source("survey/aow_module1_survey_merge_miss.R")
+source("survey/module1/aow_module1_survey_merge_miss.R")
 
 # 2. add administrative vars such as person ID, survey date and ages then basic tidy up
-source("survey/aow_module1_administrative.R")
+source("survey/module1/aow_module1_administrative.R")
 
