d70c405b1871f081cdb1650943474a213a4506da
dan.mason@bthft.nhs.uk <dan.mason@bthft.nhs.uk>
2023-07-19 14:40:51 +0100

    School measures data frame reports

---
+++ measures/measures_reports.R
@@ -0,0 +1,16 @@
+
+
+source("tools/aow_tools.R")
+
+aow_df_summary("U:/Born In Bradford - Confidential/Data/BiB/processing/AoW/measures/data/aow_bioimpedance.rds",
+               "School visit measures: bioimpedance")
+
+aow_df_summary("U:/Born In Bradford - Confidential/Data/BiB/processing/AoW/measures/data/aow_bp.rds",
+               "School visit measures: blood pressures")
+
+aow_df_summary("U:/Born In Bradford - Confidential/Data/BiB/processing/AoW/measures/data/aow_heightweight.rds",
+               "School visit measures: height and weight")
+
+aow_df_summary("U:/Born In Bradford - Confidential/Data/BiB/processing/AoW/measures/data/aow_sk.rds",
+               "School visit measures: skinfolds")
+
--- tools/aow_tools.R
+++ tools/aow_tools.R
@@ -4,7 +4,7 @@ aow_df_summary <- function(data_frame_file, data_frame_name) {
   
   rmarkdown::render("tools/describe_data_frame.Rmd", 
                     params = list(df_file = data_frame_file, df_name = data_frame_name),
-                    output_file=paste0("../reports/df_summaries/", gsub(" ", "_", tolower(data_frame_name)), ".html"))
+                    output_file=paste0("../reports/df_summaries/", gsub("[^[:alnum:]]+", "_", tolower(data_frame_name)), ".html"))
   
   
 }
