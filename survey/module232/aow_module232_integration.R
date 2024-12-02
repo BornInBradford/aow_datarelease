0a9ee64ff839378be9da785f524679ac82050846
Dan Mason <dan.mason@bthft.nhs.uk>
2024-12-02 17:00:59 +0000

    Survey integration over different annual structures now only brings forward admin vars for relevant modules so that has_ vars are correctly calculated

--- survey/module232/aow_module232_integration.R
+++ survey/module232/aow_module232_integration.R
@@ -3,21 +3,17 @@ source("tools/aow_survey_functions.R")
 
 new_module <- readRDS("U:/Born In Bradford - Confidential/Data/BiB/processing/AoW/survey/data/aow_survey_module232_linked.rds")
 
-pre_mod1 <- readRDS("U:/Born in Bradford - AOW Raw Data/previous_release/survey/aow_survey_module1_labelled.rds")
 pre_mod2 <- readRDS("U:/Born in Bradford - AOW Raw Data/previous_release/survey/aow_survey_module2_labelled.rds")
-pre_mod3 <- readRDS("U:/Born in Bradford - AOW Raw Data/previous_release/survey/aow_survey_module3_labelled.rds")
 pre_mod4 <- readRDS("U:/Born in Bradford - AOW Raw Data/previous_release/survey/aow_survey_module4_labelled.rds")
 
 # check duplicates in aow_recruitment_id in previous release
-pre_mod1$aow_recruitment_id |> duplicated() |> any()
 pre_mod2$aow_recruitment_id |> duplicated() |> any()
-pre_mod3$aow_recruitment_id |> duplicated() |> any()
 pre_mod4$aow_recruitment_id |> duplicated() |> any()
 
 new_names <- names(new_module)
 new_names_stems <- new_names |> aow_srv_var_stems()
 
-pre_rec_ids <- c(pre_mod1$aow_recruitment_id, pre_mod2$aow_recruitment_id, pre_mod3$aow_recruitment_id, pre_mod4$aow_recruitment_id) |>
+pre_rec_ids <- c(pre_mod2$aow_recruitment_id, pre_mod4$aow_recruitment_id) |>
   unique()
 
 # year_group value type has changed
@@ -38,30 +34,22 @@ format_year_group <- function(df) {
   
 }
 
-pre_mod1 <- pre_mod1 |> format_year_group()
 pre_mod2 <- pre_mod2 |> format_year_group()
-pre_mod3 <- pre_mod3 |> format_year_group()
 pre_mod4 <- pre_mod4 |> format_year_group()
 
 # derive previous module merged administrative dataset
-pre_mod_admin <- pre_mod1 |> select(any_of(aow_survey_column_order())) |>
-  bind_rows(pre_mod2 |> select(any_of(aow_survey_column_order()))) |>
-  bind_rows(pre_mod3 |> select(any_of(aow_survey_column_order()))) |>
+pre_mod_admin <- pre_mod2 |> select(any_of(aow_survey_column_order())) |>
   bind_rows(pre_mod4 |> select(any_of(aow_survey_column_order())))
 
 pre_mod_admin <- pre_mod_admin |> aow_pre_mod_admin_data()
 
 # trim previous modules down to variables carried forward and drop admin cols
-pre_mod1 <- pre_mod1 |> select(starts_with(new_names_stems)) |> select(-any_of(aow_pre_mod_drop_cols()))
 pre_mod2 <- pre_mod2 |> select(starts_with(new_names_stems)) |> select(-any_of(aow_pre_mod_drop_cols()))
-pre_mod3 <- pre_mod3 |> select(starts_with(new_names_stems)) |> select(-any_of(aow_pre_mod_drop_cols()))
 pre_mod4 <- pre_mod4 |> select(starts_with(new_names_stems)) |> select(-any_of(aow_pre_mod_drop_cols()))
 
 # merge all previous module data to be carried forward
 pre_merged <- pre_mod_admin |>
-  left_join(pre_mod1, by = "aow_recruitment_id") |>
   left_join(pre_mod2, by = "aow_recruitment_id") |>
-  left_join(pre_mod3, by = "aow_recruitment_id") |>
   left_join(pre_mod4, by = "aow_recruitment_id")
   
 # merge previous module data with new module data
