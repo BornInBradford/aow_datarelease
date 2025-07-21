5c931010e8679f257eefab91dd0d46040b2accb5
Dan Mason <dan.mason@bthft.nhs.uk>
2025-07-21 17:06:10 +0100

    Variable renaming and relabelling

---
+++ ckat/run_all.R
@@ -0,0 +1,28 @@
+
+# ParticipantInfo.rds
+# Basic participant and session details for CKAT and cognitive testing
+source("build_ckat_cog_denominator.R")
+
+# Events_data_ckat.rds
+
+# all_dvs_processeddata_ckat.rds
+
+# AIM_movement_data_ckat.rds
+# TRK_STE_Movement_data_ckat.rds
+
+
+# CogAndMotorTasksRecorded.rds
+
+# Events_data_cognitive.rds
+
+# FLK_Trials_data_cognitive.rds
+# PS_Trials_data_cognitive.rds
+# WM_Trials_data_cognitive.rds
+
+# QB_dvs_processeddata_cognitive.rds
+# RCq_dvs_processeddata_cognitive.rds
+# RCR_dvs_processeddata_cognitive.rds
+# all_dvs_processeddata_cognitive.rds
+
+
+
